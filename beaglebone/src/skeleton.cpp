6d8d3e8f92b999d37fcf92ec0fdc629bc51539fb
rummanwaqar <rumman.waqar05@gmail.com>
2019-01-16 14:50:38 -0700

    skeleton multithreaded model complete

--- beaglebone/src/skeleton.cpp
+++ beaglebone/src/skeleton.cpp
@@ -1,75 +1,74 @@
 #include <iostream>
 #include <thread>
-#include <chrono>
-#include <functional>
-#include <atomic>
 #include <mutex>
+#include <sys/time.h>
 
-using namespace std::chrono_literals;
-
-class BaseInterface {
-public:
-  BaseInterface(std::string out) : output_(out), thread_(), stop_flag_(false) {}
-  virtual ~BaseInterface() {
-    stop_flag_ = true;
-    if(thread_.joinable()) thread_.join();
-  }
-
-  virtual bool start() {
-    thread_ = std::thread(&BaseInterface::run, this);
-    return true;
-  }
-
-  virtual void run() {
-    while(!stop_flag_) {
-      std::this_thread::sleep_for(1s);
-      // set data
-      std::lock_guard<std::mutex> lock(mux_);
-      data_ = output_;
-      if(available_) {
-        std::cout << "overwritten" << std::endl;
-      }
-      available_ = true;
-
-    }
-  }
-
-  bool is_available() {
-    return available_;
-  }
-
-  std::string get_data() {
-    std::lock_guard<std::mutex> lock(mux_);
-    available_ = false;
-    return data_;
-  }
-
-private:
-  const std::string output_;
-  std::thread thread_;
-  bool stop_flag_;
-
+struct Item_t {
+  long long timestamp_;
   std::string data_;
-  std::atomic<bool> available_;
-  std::mutex mux_;
 
+  Item_t() : timestamp_(0), data_("") {}
+  Item_t(std::string data) : timestamp_(time(NULL)), data_(data) {}
 };
 
-int main() {
-  // make objects
-  BaseInterface preprocessor("Preprocessor data");
-  BaseInterface adc("Adc data");
+struct Data_t {
+  Item_t preprocessor_;
+  Item_t adc_;
+};
 
-  // start
-  preprocessor.start();
-  adc.start();
+void producer_1(Data_t& data, std::mutex& mux, std::condition_variable& convar) { // preprocessor
+  while(1) {
+    // simulate wait for data
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // got data
+    Item_t preprocessor_data("Preprocessor stuff");
+    // acquire mutex
+    std::lock_guard<std::mutex> lock(mux);
+    // write preprocessor data
+    data.preprocessor_ = preprocessor_data;
+    // if adc timestamp match
+    if(std::abs(data.adc_.timestamp_ - data.preprocessor_.timestamp_) < 1) {
+      // send notification
+      convar.notify_one();
+    }
+  }
+}
+
+void producer_2(Data_t& data, std::mutex& mux, std::condition_variable& convar) { // adc
+  while(1) {
+    // simulate wait for data
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // got data
+    Item_t adc_data("ADC stuff");
+    // acquire mutex
+    std::lock_guard<std::mutex> lock(mux);
+    // write adc data
+    data.adc_ = adc_data;
+    // if preprocessor timestamp match
+    if(std::abs(data.adc_.timestamp_ - data.preprocessor_.timestamp_) < 1) {
+      // send notification
+      convar.notify_one();
+    }
+  }
+}
+
+int main() {
+  Data_t data;
+  std::mutex mux;
+  std::condition_variable convar;
+
+  std::thread threadB{producer_2, std::ref(data), std::ref(mux), std::ref(convar)};
+  std::thread threadA{producer_1, std::ref(data), std::ref(mux), std::ref(convar)};
+
 
   while(1) {
-    // poll
-    if(preprocessor.is_available() && adc.is_available()) {
-      std::cout << preprocessor.get_data() << "\t"
-                << adc.get_data() << std::endl;
-    }
+    std::unique_lock<std::mutex> lock(mux);
+    // wait until full data object ready
+    convar.wait(lock);
+    // send data out
+    std::cout << time(NULL) << ": Got data" << "\t"
+              << data.adc_.timestamp_ << "@" << data.adc_.data_ << "\t"
+              << data.preprocessor_.timestamp_ << "@" << data.preprocessor_.data_ << std::endl;
   }
   return 0;
 }
