fe58b073400df51a13a2c8506a0ae59721088836
rummanwaqar <rumman.waqar05@gmail.com>
2019-01-17 14:54:47 -0700

    python proto build + reorganize pub

--- beaglebone/reader.py
+++ beaglebone/reader.py
@@ -1,15 +1,21 @@
 import zmq
 from google.protobuf.timestamp_pb2 import Timestamp
+from build.sonardata_pb2 import SonarData
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://127.0.0.1:9999")
 socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
-timestamp = Timestamp()
+sonar_data = SonarData()
 
 while True:
     raw_message = socket.recv()
-    timestamp.ParseFromString(raw_message)
-    print('got something')
-    print(timestamp)
+    sonar_data.ParseFromString(raw_message)
+
+    print("{}.{}".format(
+        sonar_data.timestamp.seconds,sonar_data.timestamp.nanos))
+    print(len(sonar_data.data))
+    # print(sonar_data.data[0])
+    print(sonar_data.data[0].a)
+    print(dict(sonar_data.stats))
--- beaglebone/src/protobuf_test.cpp
+++ beaglebone/src/protobuf_test.cpp
@@ -8,32 +8,69 @@
 
 #define SERVER_ADDRESS "tcp://127.0.0.1:9999"
 
+void send(std::string& output, zmq::socket_t& pub) {
+  std::size_t len = output.size();
+  zmq::message_t message(len);
+  memcpy(message.data(), output.c_str(), len);
+  pub.send(message);
+  std::cout << "Sent " << len << " bytes" << std::endl;
+}
+
+struct Data {
+  int a;
+  int refa;
+  int b;
+  int refb;
+
+  au_sonar::SonarData::Data to_proto() {
+    au_sonar::SonarData::Data data;
+    data.set_a(a);
+    data.set_refa(refa);
+    data.set_b(b);
+    data.set_refb(refb);
+    return data;
+  }
+};
 int main() {
   // Verify protobuf library version with version of headers
   GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-  au_sonar::SonarData sonar_data;
-
-  google::protobuf::Timestamp timestamp;
-  timestamp.set_seconds(time(NULL));
-  timestamp.set_nanos(0);
-
-  std::string output;
-  timestamp.SerializeToString(&output);
 
   // start zmq publisher
   zmq::context_t context(1);
   zmq::socket_t publisher(context, ZMQ_PUB);
   publisher.bind(SERVER_ADDRESS);
-
   std::cout << "Started ZMQ publisher at " << SERVER_ADDRESS << std::endl;
 
+  // gen fake data
+  Data d = {10,9,8,7};
+  au_sonar::SonarData::Data b = d.to_proto();
+  std::vector<au_sonar::SonarData::Data> data_vector;
+  for(int i=0; i<16; i++) {
+    data_vector.push_back(b);
+  }
+
+  au_sonar::SonarData sonar_data;
   while(1) {
-    std::cout << "Sending" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    zmq::message_t message(output.size());
-    memcpy(message.data(), output.c_str(), output.size());
-    publisher.send(message);
+
+    auto timestamp = sonar_data.mutable_timestamp();
+    timestamp->set_seconds(time(NULL));
+    timestamp->set_nanos(0);
+
+    // add data
+    *sonar_data.mutable_data() = {data_vector.begin(), data_vector.end()};
+
+    // add stats
+    auto map = sonar_data.mutable_stats();
+    (*map)["cal"] = 1;
+    (*map)["gain"] = 40.0;
+
+    // std::cout << sonar_data.DebugString() << std::endl;
+
+    std::string output;
+    sonar_data.SerializeToString(&output);
+    send(output, publisher);
+    sonar_data.Clear();
   }
 
   google::protobuf::ShutdownProtobufLibrary();
